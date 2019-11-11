1ee30badcc2db073dedfdf14b5b9e8656eaf13c0
Rondinele <rondineleg@edu.univali.br>
2019-11-11 13:07:34 -0300

    Interop js

---
+++ BlazorApp/dist/js/Interop.js
@@ -0,0 +1,13 @@
+﻿function criarAlert()
+{
+   alert("Ola Mundo");
+}
+function criarPrompt(question)
+{
+    return prompt(question);
+}
+
+function pegarElemento(id, text)
+{
+    document.getElementById(id).innerText = text;
+}
